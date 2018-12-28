4ca14df47a945f150c865af6ebc5fc3cd46e3d26
Oktama Pangestu <tama@jakpat.net>
2018-12-28 10:01:31 +0700

    add server.js

---
+++ dist/precache-manifest.97145ed69824036066861100ecd03f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "deeef49a20b71e8c2050",
+    "url": "/js/chunk-vendors.e929ee8c.js"
+  },
+  {
+    "revision": "9cd8d651bb5ee715dec8",
+    "url": "/js/app.e6147440.js"
+  },
+  {
+    "revision": "b74d6f306e47ef6ee7286e57e72b96da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9cd8d651bb5ee715dec8",
+    "url": "/css/app.4383fdc6.css"
+  }
+];
